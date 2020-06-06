c30805ecf68467e236b0122a00f6da92d54c8a6d
vasanth6190 <vasantharajan@codingmart.com>
2020-06-06 09:15:32 +0530

    Updates

---
+++ precache-manifest.0604552765010b380552579a9d08f7c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7b8baab7c5ab21f3f46d0e31e9ac62",
+    "url": "/medisen/index.html"
+  },
+  {
+    "revision": "6d0ce607d4150ce6bd9e",
+    "url": "/medisen/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f592ba7cd7308864ff14",
+    "url": "/medisen/static/css/main.5f842612.chunk.css"
+  },
+  {
+    "revision": "6d0ce607d4150ce6bd9e",
+    "url": "/medisen/static/js/2.4da2d3f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/medisen/static/js/2.4da2d3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f592ba7cd7308864ff14",
+    "url": "/medisen/static/js/main.4f602d3e.chunk.js"
+  },
+  {
+    "revision": "9913fe59cc79418a3132",
+    "url": "/medisen/static/js/runtime-main.7cb01805.js"
+  },
+  {
+    "revision": "ab36a5c5014a04441c227e905774687f",
+    "url": "/medisen/static/media/homepage.ab36a5c5.jpg"
+  }
+]);
